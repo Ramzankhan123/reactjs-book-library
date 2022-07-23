b48a9bb83296607b0872e550bea6235f9740fb77
Ramzan Ali <ramzan.2020@tecorb.cco>
2022-07-23 20:14:03 +0530

    redux

---
+++ src/state/reducers/index.ts
@@ -0,0 +1,13 @@
+
+
+import { combineReducers } from "redux";
+import bankReducer from "./bank-reducer"
+
+
+const reducers = combineReducers({
+    bank: bankReducer
+})
+
+export default reducers
+
+export type RootState = ReturnType<typeof reducers>
